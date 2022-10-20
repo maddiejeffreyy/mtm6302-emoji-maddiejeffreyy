4b7b172bdd4f6a6f021d9e7f4a0f99e0d2e84a63
Maddie <jeff0127@algonquinlive.com>
2022-10-20 15:02:14 -0400

    completes the assignment

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
-{/* 
-<span style="font-size: 5rem;">&#9925;</span>
-<span style="font-size: 5rem;">&#9934;</span>
-<span style="font-size: 5rem;">&#9935;</span>
-<span style="font-size: 5rem;">&#9937;</span>
-<span style="font-size: 5rem;">&#9939;</span>
-<span style="font-size: 5rem;">&#9940;</span>
-<span style="font-size: 5rem;">&#9961;</span>
-<span style="font-size: 5rem;">&#9962;</span>
-<span style="font-size: 5rem;">&#9968;</span>
-<span style="font-size: 5rem;">&#9969;</span> */}
 
-const emojis = ['&#8986;', '&#8987;', '&#9193', '&#9194', '&#9195', '&#9196', '&#9197', '&#9198', '&#9199', '&#9200',
-'&#9201;', '&#9202;', '&#9203;', '&#9208;', '&#9209;', '&#9210;', '&#9410;', '&#9748;', '&#9749;', '&#9757;', 
-'&#9801;', '&#9802;', '&#9803;', '&#9804;', '&#9805;', '&#9806;', '&#9807;', '&#9808;', '&#9809;', '&#9810;',
-'&#9811;', '&#9823;', '&#9855;', '&#9875;', '&#9889;', '&#9898;', '&#9899;', '&#9917;', '&#9918;', '&#9924;',
-'&#9925;', '&#9934;', '&#9935;', '&#9937;', '&#9939;', '&#9940;', '&#9961;', '&#9962;', '&#9968;', '&#9969;']
-title = 'Emojis'
 
-const items = [];
+// title = 'Emojis'
 
-for (const emoji of emojis){
-    items.push(`<li>${emoji}</li>`)
-}
+// const items = [];
 
-const list = `
-<h1>${title}</h1>
-<ul class ="flex">
-    ${items.join('')}
-</ul>`
+// for (const emoji of emojis){
+//     items.push(`<li>${emoji}</li>`)
+// }
+
+// const list = `
+// <h1>${title}</h1>
+// <ul class ="flex">
+//     ${items.join('')}
+// </ul>`
+
+// const $container = document.getElementById('container')
+// $container.innerHTML = list;
+
 
 const $container = document.getElementById('container')
-$container.innerHTML = list;
+
+const emojis = []
+
+for (let i = 0; i < 60; i++){
+    emojis.push(`
+    <div id="emojis" class="emojis" style="text-align: center;">
+        <div class="display">
+            <span class="emoji";">&#${i + 127780};</span><br>
+            <code class="code">${i + 127780}</code>
+        </div>
+    </div>
+    `)
+}
+
+$container.innerHTML = emojis.join('');
+
+
+  
